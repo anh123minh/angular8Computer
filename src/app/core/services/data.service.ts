0843aa65219d7ee57fc102df3cd4eafe5e35a861
anh123minh <phamanhminh231@gmail.com>
2020-05-30 18:37:22 +0300

    Important!!! Update data.service: delete .pipe()

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -5,7 +5,7 @@ import { AuthenService } from './authen.service';
 import { NotificationService } from './notification.service';
 import { UtilityService } from './utility.service';
 import { MessageContstants } from './../common/message.constants';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 
@@ -18,44 +18,35 @@ export class DataService {
     private _authenService: AuthenService,
     private _notificationService: NotificationService,
     private _utilityService: UtilityService) {
-      this.headers = this.headers.set('Content-Type', 'application/json');
-      this.headers = this.headers.set("Authorization", "Bearer " + _authenService.getLoggedInUser().access_token);
+    this.headers = this.headers.set('Content-Type', 'application/json');
+    this.headers = this.headers.set("Authorization", "Bearer " + _authenService.getLoggedInUser().access_token);
   }
 
   get(uri: string) {
-  
     return this._http.get(environment.BASE_API + uri, { headers: this.headers })
       .pipe(catchError(this.handleError));
   }
   post(uri: string, data?: any) {
-   
-    return this._http.post(environment.BASE_API + uri, data, { headers: this.headers })
-      .pipe(catchError(this.handleError));
+    return this._http.post(environment.BASE_API + uri, data, { headers: this.headers });
   }
   put(uri: string, data?: any) {
-    
-    return this._http.put(environment.BASE_API + uri, data, { headers: this.headers })
-      .pipe(catchError(this.handleError));
+    return this._http.put(environment.BASE_API + uri, data, { headers: this.headers });
   }
   delete(uri: string, key: string, id: string) {
-  
-    return this._http.delete(environment.BASE_API + uri + "/?" + key + "=" + id, { headers: this.headers })
-      .pipe(catchError(this.handleError));
+    return this._http.delete(environment.BASE_API + uri + "/?" + key + "=" + id, { headers: this.headers });
   }
   deleteWithMultiParams(uri: string, params) {
     var paramStr: string = '';
     for (let param in params) {
       paramStr += param + "=" + params[param] + '&';
     }
-    return this._http.delete(environment.BASE_API + uri + "/?" + paramStr, { headers: this.headers })
-      .pipe(catchError(this.handleError));
+    return this._http.delete(environment.BASE_API + uri + "/?" + paramStr, { headers: this.headers });
 
   }
   postFile(uri: string, data?: any) {
     let newHeader = new HttpHeaders();
     newHeader.set("Authorization", "Bearer " + this._authenService.getLoggedInUser().access_token);
-    return this._http.post(environment.BASE_API + uri, data, { headers: newHeader })
-      .pipe(catchError(this.handleError));
+    return this._http.post(environment.BASE_API + uri, data, { headers: newHeader });
   }
   public handleError(error: any) {
     if (error.status == 401) {
@@ -69,12 +60,10 @@ export class DataService {
       this._utilityService.navigateToLogin();
     }
     else {
-      let errMsg = JSON.parse(error._body).Message;
+      let errMsg = error.error.Message;
       this._notificationService.printErrorMessage(errMsg);
-
-      return Observable.throw(errMsg);
+      
+      return throwError(errMsg);
     }
-
-
   }
 }
