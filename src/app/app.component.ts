d07697adb61d919f3a1de37a7ea75a548950be25
anh123minh <phamanhminh231@gmail.com>
2020-05-30 13:41:32 +0300

    remove custom.js in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
-import { Component, AfterViewChecked, ElementRef } from '@angular/core';
+import { Component, AfterViewChecked, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewChecked {
-  constructor(private elementRef: ElementRef) {
+export class AppComponent implements OnInit {
+  constructor() {
 
   }
-  ngAfterViewChecked() {
-    var s = document.createElement("script");
-    s.type = "text/javascript";
-    s.src = "../assets/js/custom.js";
-    this.elementRef.nativeElement.appendChild(s);
+  ngOnInit() {
   }
 }
+
+
+// export class AppComponent implements AfterViewChecked {
+//   constructor(private elementRef: ElementRef) {
+
+//   }
+//   ngAfterViewChecked() {
+//     var s = document.createElement("script");
+//     s.type = "text/javascript";
+//     s.src = "../assets/js/custom.js";
+//     this.elementRef.nativeElement.appendChild(s);
+//   }
+// }
